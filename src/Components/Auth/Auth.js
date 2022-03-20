732a821c6b4e27e5d35f23e146d99dfdf6dc4a2d
Sagor Hossain <sagorhassain4@gmail.com>
2022-03-20 16:42:10 +0600

    switch mode login and signin

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Formik } from 'formik';
 
 class Auth extends Component {
+    state = {
+        mode: "Sign Up",
+    }
+    switchModeHandler = () => {
+        this.setState({
+            mode: this.state.mode === "Sign Up" ? "Login" : "Sign Up"
+        })
+    }
     render() {
         return (
             <div style={{
@@ -42,17 +50,26 @@ class Auth extends Component {
                         } else if (values.password.length < 8) {
                             errors.password = 'Must be Atlest 8 Characters';
                         }
-                        if (!values.passwordConfirm) {
-                            errors.passwordConfirm = 'Required';
-                        } else if (values.password !== values.passwordConfirm) {
-                            errors.passwordConfirm = 'Password doesnot match!';
+                        if (this.state.mode === "Sign Up") {
+                            if (!values.passwordConfirm) {
+                                errors.passwordConfirm = 'Required';
+                            } else if (values.password !== values.passwordConfirm) {
+                                errors.passwordConfirm = 'Password doesnot match!';
+                            }
                         }
-
                         console.log("Errors", errors);
                         return errors;
                     }}
                 >
                     {({ values, handleChange, handleSubmit, errors }) => (<div>
+                        <button style={{
+                            width: "100%",
+                            backgroundColor: "orange",
+                            color: "#D70F64",
+                            marginBottom: "10px",
+                            borderRadius: "5px",
+                        }} className="btn btn-lg" onClick={this.switchModeHandler}>Switch to {this.state.mode === "Sign Up" ? "Login" : "Sign Up"}</button>
+                        <br />
                         <form onSubmit={handleSubmit}>
                             <input
                                 name="email"
@@ -72,16 +89,22 @@ class Auth extends Component {
                             />
                             {errors.password}
                             <br />
-                            <input
-                                name="passwordConfirm"
-                                placeholder="Confirm Password"
-                                className="form-control"
-                                value={values.passwordConfirm}
-                                onChange={handleChange}
-                            />
-                            {errors.passwordConfirm}
-                            <br />
-                            <button type="submit" className="btn btn-success">Sign Up</button>
+                            {this.state.mode === "Sign Up" ?
+                                <div>
+                                    <input
+                                        name="passwordConfirm"
+                                        placeholder="Confirm Password"
+                                        className="form-control"
+                                        value={values.passwordConfirm}
+                                        onChange={handleChange}
+                                    />
+                                    {errors.passwordConfirm}
+                                    <br />
+                                </div> : null
+                            }
+
+                            <button type="submit" className="btn btn-success">{this.state.mode === "Sign Up" ? "Sign Up" : "Login"}</button>
+
                         </form>
                     </div>)}
                 </Formik>
