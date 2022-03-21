34926d75c8e717d6d2dfcb973947c9973f80c5e2
Sagor Hossain <sagorhassain4@gmail.com>
2022-03-22 00:14:51 +0600

    redux store upate for auth in main js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header/Header';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 import Orders from './Orders/Orders';
@@ -6,6 +6,8 @@ import Checkout from './Orders/Checkout/Checkout';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './Auth/Auth';
 import { connect } from 'react-redux';
+import { authCheck } from '../redux/authActionCreator';
+
 
 const mapStateToProps = state => {
     return {
@@ -13,34 +15,46 @@ const mapStateToProps = state => {
     }
 }
 
-const Main = props => {
-    let routes = null;
-    if (props.token === null) {
-        routes = (
-            <Routes>
-                <Route path="/login" element={<Auth />} />
-                <Route path="*" element={<Navigate to="/login" />} />
-            </Routes>
-        )
-    } else {
-        routes = (
-            <Routes>
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/" exact element={<BurgerBuilder />} />
-                <Route path="/login" element={<Navigate to="/" />} />
-            </Routes>
-        )
+const mapDispatchToProps = dispatch => {
+    return {
+        authCheck: () => dispatch(authCheck()),
     }
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                {routes}
-            </div>
-
-        </div>
-    )
 }
 
-export default connect(mapStateToProps)(Main);
+class Main extends Component {
+    componentDidMount() {
+        this.props.authCheck();
+    }
+    render() {
+        let routes = null;
+        if (this.props.token === null) {
+            routes = (
+                <Routes>
+                    <Route path="/login" element={<Auth />} />
+                    <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+            )
+        } else {
+            routes = (
+                <Routes>
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/" exact element={<BurgerBuilder />} />
+                    <Route path="/login" element={<Navigate to="/" />} />
+                </Routes>
+            )
+        }
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    {routes}
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
